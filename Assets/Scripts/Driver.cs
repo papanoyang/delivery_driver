f3b67290e6f245a548b02a65c9cb8225a3ef68d1
YangsNote <yangkwangho@gmail.com>
2023-06-03 23:11:49 +0900

    デリバリー完成

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField]
+    Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField]
+    Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
     [SerializeField] float steerSpeed = 1f;
     [SerializeField] float moveSpeed = 0.01f;
+
+    private bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -16,12 +29,19 @@ public class Driver : MonoBehaviour
         transform.Translate(0, moveAmount, 0);
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("キャー");
-    }
-
-    private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Pass!");
+        if (other.tag == "Uper" && hasPackage == false)
+        {
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, 0.5f);
+        }
+        if (other.tag == "Customer")
+        {
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+            Destroy(other.gameObject, 0.5f);
+        }
     }
 }
